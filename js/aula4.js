0bdd67c9ccec34da08b4b44cb422e8aed938daa6
guilherme maywald <guilhermemaywald@gmail.com>
2022-08-31 14:45:55 -0300

    Atualizando código

--- js/aula4.js
+++ js/aula4.js
@@ -1,14 +1,15 @@
-const prompt = require('prompt-sync')();
+{
+    let recebeLetra = prompt('Digite uma letra do alfabeto:');
+    recebeLetra = recebeLetra.toLowerCase();
 
-let letraAlfabeto = prompt('Digite uma letra do alfabeto: ');
-if (letraAlfabeto == 'a'
-    || letraAlfabeto == 'e'
-    || letraAlfabeto == 'i'
-    || letraAlfabeto == 'o'
-    || letraAlfabeto == 'u') {
-        return console.log('Letra digitada é uma vogal!');
-    } else if (letraAlfabeto = ['b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']) {
-        return console.log('A letra é uma consoante!');
-    } else {
-        console.log('Digite uma letra!!!');
+    if(recebeLetra.length>1){
+        alert("Você deve digitar apenas uma letra.");
+    }else if(!isNaN(recebeLetra)){  
+        alert("Você não deve digitar um número!");
+    }else if(recebeLetra=='a' || recebeLetra=='e' || recebeLetra=='i' 
+    || recebeLetra =='o' || recebeLetra=='u' ){
+        alert('A letra digitada é uma vogal');
+    }else {
+        alert ('A letra digitada é uma consoante');
     }
+}
